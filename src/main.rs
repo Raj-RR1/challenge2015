49fa09b43af38371d6d826733a24d65cc99fb6c9
Raj Raorane <rajraorane576@gmail.com>
2025-03-15 13:44:01 +0530

    map json to struct
    * add dependencies
    * parse cli arguments for actor1 and actor2
    * define structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+pub struct Actor {
+    url: String,
+    name: String,
+    movies: Vec<ActorDetails>,
+}
+
+pub struct ActorDetails {
+    name: String,
+    url: String,
+    role: String,
+}
+
+pub struct Movie{
+    url: String,
+    name: String,
+    cast: Vec<MovieDetails>
+}
+
+pub struct MovieDetails{
+   url: String,
+   name: String,
+   role: String,
+}
+
+#[derive(Parser)]
+#[command(name = "moviebuff-cli")]
+#[command(
+    about = "Figure out the smallest degree of seperation between two people using the Moviebuff data"
+)]
+struct Cli {
+    actor1: String,
+    actor2: String,
+}
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    println!(
+        "Figuring out the smallest degree of seperation between {} and {}",
+        cli.actor1, cli.actor2
+    );
 }
