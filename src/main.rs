5b29de1fa3c4e4d4d3392e55d32fc8b253332a6d
Raj Raorane <rajraorane576@gmail.com>
2025-03-16 21:40:05 +0530

    logic to find degrees of seperation
    * add fields cast and crew for Movie Struct.
    * derive trait Clone for Movie Details.
    * some print line for debugging
    * test out fetching movies directly from main
    * rename parameter url to name in fetch_movie_json
    * commenting fn degrees_of_seperation for now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, io::Read};
+use std::{collections::{HashSet, VecDeque}, error::Error, io::Read};
 
 use clap::Parser;
 use flate2::read::GzDecoder;
@@ -27,10 +27,11 @@ pub struct Movie{
     #[serde(rename="type")]
     movie_type: String,
     name: String,
-    cast: Vec<MovieDetails>
+    cast: Vec<MovieDetails>,
+    crew: Vec<MovieDetails>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug,Clone)]
 pub struct MovieDetails{
    url: String,
    name: String,
@@ -56,6 +57,7 @@ async fn fetch_actor_json(name: &str) -> Result<Actor, Box<dyn Error>>{
     let response = client.get(url).header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)").header("Accept-Encoding", "gzip").send().await?;
 
     if !response.status().is_success() {
+        println!("HTTP error comes while fetching actor");
         return Err(format!("HTTP Error: {}", response.status()).into());
     }
 
@@ -69,12 +71,15 @@ async fn fetch_actor_json(name: &str) -> Result<Actor, Box<dyn Error>>{
     Ok(json)
 }
 
-async fn fetch_movie_json(url: &str) -> Result<Movie, Box<dyn Error>>{
+async fn fetch_movie_json(name: &str) -> Result<Movie, Box<dyn Error>>{
 
+    let url = format!("https://data.moviebuff.com/{}",name);
     let client = Client::new();
-    let response = client.get(url).header("User-Agent", "Mozilla/5.0").header("Accept-Encoding", "gzip").send().await?;
+
+    let response = client.get(url).header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)").header("Accept-Encoding", "gzip").send().await?;
 
     if !response.status().is_success() {
+        println!("HTTP error comes while fetching movie");
          return Err(format!("HTTP Error: {}", response.status()).into());
     }
 
@@ -87,6 +92,51 @@ async fn fetch_movie_json(url: &str) -> Result<Movie, Box<dyn Error>>{
     Ok(json)
 }
 
+async fn find_degrees_of_separation(actor1: String, actor2: String) -> Result<(), Box<dyn Error>>{
+
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    let in_place :Vec<(String, (String, String), (String, String))> = vec![];
+    queue.push_back((actor1.clone(),in_place));
+    visited.insert(actor1.to_string());
+
+    while let Some((current_actor, path)) = queue.pop_front(){
+
+        let actor_data = fetch_actor_json(&current_actor).await?; // this will fetch the actor_url, type(Person), name and the movies for that actor.
+        // println!("After actor data fetch");
+
+        for movie in actor_data.movies.iter() { // Iterate over all the movies the actor has worked in.
+
+            let movie_data = fetch_movie_json(&movie.url).await?; // this will fetch the movie_url, type(Movie), name and the cast of current movie.
+
+            // println!("After movie_data fetch");
+
+            for co_actor in  movie_data.cast.into_iter(){ // Iterate over all the cast in this movie.
+
+                if co_actor.url == actor2 {
+                    println!("Degrees of Seperation: {}", path.len() + 1);
+                    for (i,(m, a1, a2)) in path.iter().enumerate()  {
+                     println!("{}. Movie: {}", i+1, m);
+                     println!("{}:{}", a1.0, a1.1);
+                     println!("{}:{}", a2.0, a2.1);
+                    }
+
+                    return Ok(());
+                }
+
+                if !visited.contains(&co_actor.url) {
+
+                    let mut new_path = path.clone();
+                    new_path.push((movie.name.clone(),(movie.role.clone(),actor_data.name.clone()), (co_actor.role.clone(), co_actor.name.clone())));
+                    queue.push_back((co_actor.url.clone(), new_path));
+                    visited.insert(co_actor.url.clone());
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -96,10 +146,15 @@ async fn main() {
         cli.actor1, cli.actor2
     );
 
-    match fetch_actor_json(&cli.actor1).await {
+    // match find_degrees_of_separation(cli.actor1, cli.actor2).await {
+    //     Ok(_) => {},
+    //     Err(e) => eprintln!("Error: {}", e),
+    // }
+
+    match fetch_movie_json("taxi-driver").await {
 
      Ok(json) => {
-             println!("Actor JSON: {:?}", json);
+             println!("Movie JSON: {:?}", json);
      }
      Err(e) =>{
          eprintln!("Failed to fetch actor data: {}", e);
